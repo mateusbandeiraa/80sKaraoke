00f6967248bc7f24e4daf8bf1d66f5c9a796ca48
mateusbandeiraa <mateusbandeiraa@gmail.com>
2020-05-09 19:19:23 -0300

    Update homepage to show featured tracks

--- src/main/webapp/homepage-script.js
+++ src/main/webapp/homepage-script.js
@@ -7,59 +7,84 @@ const searchterms = urlParams.get("searchterms");
 document.addEventListener("DOMContentLoaded", async function () {
     let searchElement = document.querySelector("#search-form #tracktitle");
     searchElement.addEventListener("input", performSearch);
-    if(searchterms){
+    if (searchterms) {
         searchElement.value = searchterms;
         searchElement.dispatchEvent(new Event("input"));
     }
+
+    const featuredTracks = await fetchFeaturedTracks();
+    updateFeaturedTracks(featuredTracks);
 });
 
-function performSearch(event) {
+async function performSearch(event) {
     let element = event.target;
     let searchTerms = element.value;
     if (searchTerms && searchTerms.length >= 3) {
-        fetchSearchTracks(searchTerms);
+        const searchResults = await fetchSearchTracks(searchTerms);
+        updateTrackResults(searchResults);
     } else {
         clearSearch();
     }
 }
-function clearSearch(){
+function clearSearch() {
     let resultsContainer = document.getElementById("track-search-results-container");
     resultsContainer.innerHTML = "";
 }
-async function fetchSearchTracks(searchTerms) {
+function fetchSearchTracks(searchTerms) {
     let urlSearchParams = new URLSearchParams();
     urlSearchParams.set("searchterms", searchTerms);
     const url = `${API_ENDPOINT}/track/search?${urlSearchParams.toString()}`;
-    const searchResults = await fetch(url).then((data) => data.json());
-    updateTrackResults(searchResults);
+    const searchResults = fetch(url).then((data) => data.json());
+    return searchResults;
+}
+
+function fetchFeaturedTracks() {
+    const url = `${API_ENDPOINT}/track/featured`;
+    const featuredTracks = fetch(url).then((data) => data.json());
+    return featuredTracks;
 }
 
 function updateTrackResults(tracks) {
     let resultsContainer = document.getElementById("track-search-results-container");
     clearSearch();
     for (let track of tracks) {
-        let trackSearchResultElement = document.createElement("a");
-        trackSearchResultElement.classList.add("track-search-result");
-        trackSearchResultElement.href = `/play?trackid=${track.trackId}`;
-
-        let titleElement = document.createElement("span");
-        titleElement.classList.add("track-search-result-title");
-        titleElement.textContent = track.title;
-
-        let artistElement = document.createElement("span");
-        artistElement.classList.add("track-search-result-artist");
-        artistElement.textContent = track.artist;
-
-        let yearElement = document.createElement("span");
-        yearElement.classList.add("track-search-result-year");
-        yearElement.textContent = track.year;
-
-        trackSearchResultElement.appendChild(titleElement);
-        trackSearchResultElement.innerHTML += " — ";
-        trackSearchResultElement.appendChild(artistElement);
-        trackSearchResultElement.innerHTML += " (";
-        trackSearchResultElement.appendChild(yearElement);
-        trackSearchResultElement.innerHTML += ")";
+        let trackSearchResultElement = createTrackLinkElement(track);
         resultsContainer.appendChild(trackSearchResultElement);
     }
+}
+
+function updateFeaturedTracks(tracks){
+    let featuredTracksContainer = document.getElementById("featured-tracks-container");
+    featuredTracksContainer.innerHTML = "";
+    for (let track of tracks) {
+        let featuredTrackElement = createTrackLinkElement(track);
+        featuredTracksContainer.appendChild(featuredTrackElement);
+    }
+}
+
+function createTrackLinkElement(track) {
+    let trackLinkElement = document.createElement("a");
+    trackLinkElement.classList.add("track-link");
+    trackLinkElement.href = `/play?trackid=${track.trackId}`;
+
+    let titleElement = document.createElement("span");
+    titleElement.classList.add("track-link-title");
+    titleElement.textContent = track.title;
+
+    let artistElement = document.createElement("span");
+    artistElement.classList.add("track-link-artist");
+    artistElement.textContent = track.artist;
+
+    let yearElement = document.createElement("span");
+    yearElement.classList.add("track-link-year");
+    yearElement.textContent = track.year;
+
+    trackLinkElement.appendChild(titleElement);
+    trackLinkElement.innerHTML += " — ";
+    trackLinkElement.appendChild(artistElement);
+    trackLinkElement.innerHTML += " (";
+    trackLinkElement.appendChild(yearElement);
+    trackLinkElement.innerHTML += ")";
+
+    return trackLinkElement;
 }
